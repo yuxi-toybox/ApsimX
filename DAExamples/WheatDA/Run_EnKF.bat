fbde146dcc15d6f013d3ad074c19edc6aa54f7bb
Yuxi <23029366+yuxi-toybox@users.noreply.github.com>
2022-07-31 00:09:37 +1000

    A data assimilation example.
    Added a data assimilation example under ApsimX\DAExamples with a documentation draft.
    Note: absolute paths must be changed to relative paths.
    
    Co-Authored-By: Yuxi Zhang <77602804+yuxi-research@users.noreply.github.com>

---
+++ DAExamples/WheatDA/Run_EnKF.bat
@@ -0,0 +1,3 @@
+D:\Works\GitHub_CropDA\DABranch3\ApsimX\bin\Debug\netcoreapp3.1\Models.exe Output\EnKF.apsimx
+
+timeout 3
---
+++ DAExamples/WheatDA/Run_OpenLoop.bat
@@ -0,0 +1,3 @@
+D:\Works\GitHub_CropDA\DABranch3\ApsimX\bin\Debug\netcoreapp3.1\Models.exe Output\OpenLoop.apsimx
+
+timeout 3
